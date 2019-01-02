8247926c0b71fd4b86ba104c4e56f69f1881ba64
springnewlearner <1378770760@qq.com>
2019-01-02 09:23:29 +0800

    ex29-30 commit

---
+++ 1 Fixed-Size Data/30.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |30|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+
+(define TICKET-PRICE 3)
+(define ATTENDEES
+  (- 120 (* (- TICKET-PRICE 5.0) (/ 15 0.1))))
+(define REVENUE
+  (* TICKET-PRICE ATTENDEES))
+(define COST
+  (* 1.5 ATTENDEES))
+(define PROFIT
+  (- REVENUE COST))
